2124585641813d5db03b9abb999f40ce0652b936
Bastien Joyeau <bastien.joyeau@student.vinci.be>
2024-11-13 21:07:09 +0100

    refactor of language used in descriptions

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,173 +1,173 @@
 import { Main } from "../src/app.js";
 
 describe("getOneMinuteRow function should return the Berlin simple minutes of a digital hour", function () {
-  const main = new Main();
+    const main = new Main();
 
-  it("Test de 0 simple minutes", function () {
-    const result = main.getOneMinuteRow(0);
-    expect(result).toBe("OOOO");
-  });
+    it("Test of 0 simple minutes", function () {
+        const result = main.getOneMinuteRow(0);
+        expect(result).toBe("OOOO");
+    });
 
-  it("Test de 1 simple minutes", function () {
-    const result = main.getOneMinuteRow(1);
-    expect(result).toBe("YOOO");
-  });
+    it("Test of 1 simple minute", function () {
+        const result = main.getOneMinuteRow(1);
+        expect(result).toBe("YOOO");
+    });
 
-  it("Test de 2 simple minutes", function () {
-    const result = main.getOneMinuteRow(2);
-    expect(result).toBe("YYOO");
-  });
+    it("Test of 2 simple minutes", function () {
+        const result = main.getOneMinuteRow(2);
+        expect(result).toBe("YYOO");
+    });
 
-  it("Test de 3 simple minutes", function () {
-    const result = main.getOneMinuteRow(3);
-    expect(result).toBe("YYYO");
-  });
+    it("Test of 3 simple minutes", function () {
+        const result = main.getOneMinuteRow(3);
+        expect(result).toBe("YYYO");
+    });
 
-  it("Test de 4 simple minutes", function () {
-    const result = main.getOneMinuteRow(4);
-    expect(result).toBe("YYYY");
-  });
+    it("Test of 4 simple minutes", function () {
+        const result = main.getOneMinuteRow(4);
+        expect(result).toBe("YYYY");
+    });
 });
 
 describe("getFiveMinutesRow function should return the Berlin five minutes of a digital hour", function () {
-  const main = new Main();
+    const main = new Main();
 
-  it("Test de 0 minutes", function () {
-    const result = main.getFiveMinutesRow(0);
-    expect(result).toBe("OOOOOOOOOOO");
-  });
+    it("Test of 0 minutes", function () {
+        const result = main.getFiveMinutesRow(0);
+        expect(result).toBe("OOOOOOOOOOO");
+    });
 
-  it("Test de 5 minutes", function () {
-    const result = main.getFiveMinutesRow(5);
-    expect(result).toBe("YOOOOOOOOOO");
-  });
+    it("Test of 5 minutes", function () {
+        const result = main.getFiveMinutesRow(5);
+        expect(result).toBe("YOOOOOOOOOO");
+    });
 
-  it("Test de 10 minutes", function () {
-    const result = main.getFiveMinutesRow(10);
-    expect(result).toBe("YYOOOOOOOOO");
-  });
+    it("Test of 10 minutes", function () {
+        const result = main.getFiveMinutesRow(10);
+        expect(result).toBe("YYOOOOOOOOO");
+    });
 
-  it("Test de 15 minutes", function () {
-    const result = main.getFiveMinutesRow(15);
-    expect(result).toBe("YYROOOOOOOO");
-  });
+    it("Test of 15 minutes", function () {
+        const result = main.getFiveMinutesRow(15);
+        expect(result).toBe("YYROOOOOOOO");
+    });
 
-  it("Test de 20 minutes", function () {
-    const result = main.getFiveMinutesRow(20);
-    expect(result).toBe("YYRYOOOOOOO");
-  });
+    it("Test of 20 minutes", function () {
+        const result = main.getFiveMinutesRow(20);
+        expect(result).toBe("YYRYOOOOOOO");
+    });
 
-  it("Test de 25 minutes", function () {
-    const result = main.getFiveMinutesRow(25);
-    expect(result).toBe("YYRYYOOOOOO");
-  });
+    it("Test of 25 minutes", function () {
+        const result = main.getFiveMinutesRow(25);
+        expect(result).toBe("YYRYYOOOOOO");
+    });
 
-  it("Test de 30 minutes", function () {
-    const result = main.getFiveMinutesRow(30);
-    expect(result).toBe("YYRYYROOOOO");
-  });
+    it("Test of 30 minutes", function () {
+        const result = main.getFiveMinutesRow(30);
+        expect(result).toBe("YYRYYROOOOO");
+    });
 
-  it("Test de 35 minutes", function () {
-    const result = main.getFiveMinutesRow(35);
-    expect(result).toBe("YYRYYRYOOOO");
-  });
+    it("Test of 35 minutes", function () {
+        const result = main.getFiveMinutesRow(35);
+        expect(result).toBe("YYRYYRYOOOO");
+    });
 
-  it("Test de 40 minutes", function () {
-    const result = main.getFiveMinutesRow(40);
-    expect(result).toBe("YYRYYRYYOOO");
-  });
+    it("Test of 40 minutes", function () {
+        const result = main.getFiveMinutesRow(40);
+        expect(result).toBe("YYRYYRYYOOO");
+    });
 
-  it("Test de 45 minutes", function () {
-    const result = main.getFiveMinutesRow(45);
-    expect(result).toBe("YYRYYRYYROO");
-  });
+    it("Test of 45 minutes", function () {
+        const result = main.getFiveMinutesRow(45);
+        expect(result).toBe("YYRYYRYYROO");
+    });
 
-  it("Test de 50 minutes", function () {
-    const result = main.getFiveMinutesRow(50);
-    expect(result).toBe("YYRYYRYYRYO");
-  });
+    it("Test of 50 minutes", function () {
+        const result = main.getFiveMinutesRow(50);
+        expect(result).toBe("YYRYYRYYRYO");
+    });
 
-  it("Test de 55 minutes", function () {
-    const result = main.getFiveMinutesRow(55);
-    expect(result).toBe("YYRYYRYYRYY");
-  });
+    it("Test of 55 minutes", function () {
+        const result = main.getFiveMinutesRow(55);
+        expect(result).toBe("YYRYYRYYRYY");
+    });
 });
 
 describe("getOneHourRow function should return the Berlin simple hours of a digital hour", function () {
-  const main = new Main();
+    const main = new Main();
 
-  it("Test de 0 simple heure", function () {
-    const result = main.getOneHourRow(0);
-    expect(result).toBe("OOOO");
-  });
-  it("Test de 1 simple heures", function () {
-    const result = main.getOneHourRow(1);
-    expect(result).toBe("ROOO");
-  });
-  it("Test de 2 simple heures", function () {
-    const result = main.getOneHourRow(2);
-    expect(result).toBe("RROO");
-  });
-  it("Test de 3 simple heures", function () {
-    const result = main.getOneHourRow(3);
-    expect(result).toBe("RRRO");
-  });
-  it("Test de 4 simple heures", function () {
-    const result = main.getOneHourRow(4);
-    expect(result).toBe("RRRR");
-  });
+    it("Test of 0 simple hours", function () {
+        const result = main.getOneHourRow(0);
+        expect(result).toBe("OOOO");
+    });
+    it("Test of 1 simple hour", function () {
+        const result = main.getOneHourRow(1);
+        expect(result).toBe("ROOO");
+    });
+    it("Test of 2 simple hours", function () {
+        const result = main.getOneHourRow(2);
+        expect(result).toBe("RROO");
+    });
+    it("Test of 3 simple hours", function () {
+        const result = main.getOneHourRow(3);
+        expect(result).toBe("RRRO");
+    });
+    it("Test of 4 simple hours", function () {
+        const result = main.getOneHourRow(4);
+        expect(result).toBe("RRRR");
+    });
 });
 
 describe("getFiveHourRow function should return the Berlin Five hours of a digital hour", function () {
-  const main = new Main();
+    const main = new Main();
 
-  it("Test of 0  heure", function () {
-    const result = main.getFiveHoursRow(0);
-    expect(result).toBe("OOOO");
-  });
-  it("Test de 5 heures", function () {
-    const result = main.getFiveHoursRow(5);
-    expect(result).toBe("ROOO");
-  });
-  it("Test de 10 heures", function () {
-    const result = main.getFiveHoursRow(10);
-    expect(result).toBe("RROO");
-  });
-  it("Test de 15 simple heures", function () {
-    const result = main.getFiveHoursRow(15);
-    expect(result).toBe("RRRO");
-  });
-  it("Test de 20 simple heures", function () {
-    const result = main.getFiveHoursRow(20);
-    expect(result).toBe("RRRR");
-  });
+    it("Test of 0 hours", function () {
+        const result = main.getFiveHoursRow(0);
+        expect(result).toBe("OOOO");
+    });
+    it("Test of 5 hours", function () {
+        const result = main.getFiveHoursRow(5);
+        expect(result).toBe("ROOO");
+    });
+    it("Test of 10 hours", function () {
+        const result = main.getFiveHoursRow(10);
+        expect(result).toBe("RROO");
+    });
+    it("Test of 15 hours", function () {
+        const result = main.getFiveHoursRow(15);
+        expect(result).toBe("RRRO");
+    });
+    it("Test of 20 hours", function () {
+        const result = main.getFiveHoursRow(20);
+        expect(result).toBe("RRRR");
+    });
 });
 
 describe("getSeconds function should return the Berlin seconds of a digital hour", function () {
-  const main = new Main();
+    const main = new Main();
 
-  it("Test de 0 secondes", function () {
-    const result = main.getSeconds(0);
-    expect(result).toBe("R");
-  });
+    it("Test of 0 seconds", function () {
+        const result = main.getSeconds(0);
+        expect(result).toBe("R");
+    });
 
-  it("Test de 1 secondes", function () {
-    const result = main.getSeconds(1);
-    expect(result).toBe("O");
-  });
+    it("Test of 1 second", function () {
+        const result = main.getSeconds(1);
+        expect(result).toBe("O");
+    });
 
-  it("Test de 2 secondes", function () {
-    const result = main.getSeconds(2);
-    expect(result).toBe("R");
-  });
+    it("Test of 2 seconds", function () {
+        const result = main.getSeconds(2);
+        expect(result).toBe("R");
+    });
 });
 
 describe("getWholeClock function should return the Berlin clock of a digital hour", function () {
-  const main = new Main();
+    const main = new Main();
 
-  it("Test de 20:36:37", function () {
-    const result = main.getWholeClock(70597);
-    expect(result).toBe("O\nRRRR\nOOOO\nYYRYYRYOOOO\nYOOO");
-  });
+    it("Test of 20:36:37", function () {
+        const result = main.getWholeClock(70597);
+        expect(result).toBe("O\nRRRR\nOOOO\nYYRYYRYOOOO\nYOOO");
+    });
 
 });
